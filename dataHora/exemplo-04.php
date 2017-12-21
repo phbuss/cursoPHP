87ebee113bcdd85d51460cbfc635747084820c82
Paulo Henrique Buss <paulo.buss@gmail.com>
2017-12-21 12:54:12 -0200

    Data e hora

--- dataHora/exemplo-04.php
+++ dataHora/exemplo-04.php
@@ -1,3 +1,10 @@
 <?php
-  teste
+  $dt = new DateTime();
+  echo "Data: " . $dt->format("d/m/Y H:i:s") . "<br>";
+
+  $periodo = new DateInterval("P15D");
+
+  $dt->add($periodo);
+
+  echo "Data + 15: " . $dt->format("d/m/Y H:i:s") . "<br>";
 ?>
