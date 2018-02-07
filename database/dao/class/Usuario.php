0ad46e537ddce8633703235a1e973c45ac36d05c
Paulo Henrique Buss <paulo.buss@gmail.com>
2018-02-07 12:47:02 -0200

    DAO métodos de lista

--- database/dao/class/Usuario.php
+++ database/dao/class/Usuario.php
@@ -51,11 +51,37 @@ class Usuario {
     }
   }
 
-  public function getList() {
+  public static function getList() {
     $sql = new Sql();
     return $sql->select("select * from tb_usuarios order by dslogin");
   }
 
+  public static function searchByLogin($login) {
+    $sql = new Sql();
+    return $sql->select("select * from tb_usuarios where dslogin like :dslogin order by dslogin", array(
+        ":dslogin" => "%".$login."%"
+    ));
+  }
+  
+  public function login($login, $senha) {
+    $sql = new Sql();
+    $result = $sql->select("select * from tb_usuarios where dslogin = :login and dssenha = :senha",array(
+        ":login"=>$login,
+        ":senha"=>$senha
+    ));
+    
+    if (count($result) > 0) {
+      $row = $result[0];
+      $this->setIdusuario($row["idusuario"]);
+      $this->setDslogin($row["dslogin"]);
+      $this->setDssenha($row["dssenha"]);
+      $this->setDtcadastro(new DateTime($row["dtcadastro"]));
+    } else {
+      throw new Exception ("Login e/ou senha inválidos");
+    }
+  }
+  
+  
   public function __toString() {
     return json_encode(array(
         "idusuario" => $this->getIdusuario(),
--- database/dao/index.php
+++ database/dao/index.php
@@ -1,9 +1,21 @@
 <?php
 require_once 'config.php';
 
-/*
-//Carrega um usuário
+echo "* Usuário específico **************************" . "<br>";
 $usuario = new Usuario();
 $usuario->loadById(1);
 echo $usuario;
-*/
+
+echo "<br>" . "* Lista TODOS os usuários **************************" . "<br>";
+$usuarios = Usuario::getList();
+echo json_encode($usuarios);
+
+echo "<br>" ."* Lista de usuários por login **************************" . "<br>";
+$usuarios = Usuario::searchByLogin("u");
+echo json_encode($usuarios);
+
+echo "<br>" ."* Login **************************" . "<br>";
+$usuario = new Usuario();
+//$usuario->login("root","AAA"); //Erro
+$usuario->login("root","root"); //OS
+echo $usuario . "<br>";
